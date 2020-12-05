ff06cf664368d9816811a2f1566cc3bbcb1479f9
Sinordi <Sergei_009_08@mail.ru>
2020-12-05 17:05:57 +0500

    Version 1

---
+++ Piatnashki/PiatnashkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiatnashkiApp.swift
+//  Piatnashki
+//
+//  Created by Сергей Кривошапко on 05.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct PiatnashkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
